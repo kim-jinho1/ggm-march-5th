5c04f8928ed0011a12342e3ff725b61c8cfbd588
김진호 <russiakim2008@gmail.com>
2025-03-07 22:32:49 +0900

    project

---
+++ GitGGM/GitGGM/Form2.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace GitGGM
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.url = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Pushbtn = new System.Windows.Forms.Button();
+            this.Commitbtn = new System.Windows.Forms.Button();
+            this.txtCommitMessage = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(163, 131);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(446, 21);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // url
+            // 
+            this.url.Location = new System.Drawing.Point(163, 104);
+            this.url.Name = "url";
+            this.url.Size = new System.Drawing.Size(446, 21);
+            this.url.TabIndex = 0;
+            this.url.TextChanged += new System.EventHandler(this.url_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(615, 129);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "파일찾기";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(275, 227);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Clone";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Pushbtn
+            // 
+            this.Pushbtn.Location = new System.Drawing.Point(408, 227);
+            this.Pushbtn.Name = "Pushbtn";
+            this.Pushbtn.Size = new System.Drawing.Size(75, 23);
+            this.Pushbtn.TabIndex = 3;
+            this.Pushbtn.Text = "Push";
+            this.Pushbtn.UseVisualStyleBackColor = true;
+            this.Pushbtn.Click += new System.EventHandler(this.Pushbtn_Click);
+            // 
+            // Commitbtn
+            // 
+            this.Commitbtn.Location = new System.Drawing.Point(534, 227);
+            this.Commitbtn.Name = "Commitbtn";
+            this.Commitbtn.Size = new System.Drawing.Size(75, 23);
+            this.Commitbtn.TabIndex = 4;
+            this.Commitbtn.Text = "Commit";
+            this.Commitbtn.UseVisualStyleBackColor = true;
+            this.Commitbtn.Click += new System.EventHandler(this.Commitbtn_Click);
+            // 
+            // txtCommitMessage
+            // 
+            this.txtCommitMessage.Location = new System.Drawing.Point(163, 158);
+            this.txtCommitMessage.Name = "txtCommitMessage";
+            this.txtCommitMessage.Size = new System.Drawing.Size(446, 21);
+            this.txtCommitMessage.TabIndex = 5;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtCommitMessage);
+            this.Controls.Add(this.Commitbtn);
+            this.Controls.Add(this.Pushbtn);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.url);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox url;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button Pushbtn;
+        private System.Windows.Forms.Button Commitbtn;
+        private System.Windows.Forms.TextBox txtCommitMessage;
+    }
+}
---
+++ GitGGM/GitGGM/Form2.cs
@@ -0,0 +1,186 @@
+﻿using LibGit2Sharp;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace GitGGM
+{
+    public partial class Form2 : Form
+    {
+        string repoUrl = null;
+        string localPath = null;
+        Repository repo = new Repository();
+        private string _directory = null;
+
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)  
+        {
+            Text = localPath;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog())
+            {
+                folderBrowserDialog.Description = "폴더를 선택하세요";
+                folderBrowserDialog.ShowNewFolderButton = true;
+
+                if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+                {
+                    localPath = folderBrowserDialog.SelectedPath;
+                    textBox1.Text = localPath;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            repoUrl = url.Text;
+            localPath = Path.Combine(localPath, "GitGGM");
+
+            if (Directory.Exists(localPath))
+            {
+                try
+                {
+                    Directory.Delete(localPath, true);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"기존 폴더 삭제 실패: {ex.Message}");
+                    return;
+                }
+            }
+
+            try
+            {
+                Repository.Clone(repoUrl, localPath);
+                MessageBox.Show("클론 성공!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"클론 실패: {ex.Message}");
+            }
+        }
+
+        private void url_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Pushbtn_Click(object sender, EventArgs e)
+        {
+            // 푸시 실행
+            string pushOutput = RunGitCommand(_directory, "push origin main");
+            if (string.IsNullOrEmpty(pushOutput))
+            {
+                MessageBox.Show("푸시 실패! 서버 확인이 필요합니다.");
+            }
+            else
+            {
+                MessageBox.Show("푸시 완료!");
+            }
+
+            txtCommitMessage.Clear();
+        }
+
+        private void Commitbtn_Click(object sender, EventArgs e)
+        {
+            string commitMessage = txtCommitMessage.Text.Trim();
+            if (string.IsNullOrWhiteSpace(commitMessage))
+            {
+                MessageBox.Show("커밋 메시지를 입력하세요.");
+                return;
+            }
+
+            string directory = @"D:\C#\GitGGM\GitGGM";
+            _directory = directory;
+            // Git 저장소가 아니면 초기화
+            if (!IsGitRepository(directory))
+            {
+                InitializeGitRepository(directory);
+            }
+
+            // 변경된 파일을 추가하고 커밋을 수행
+            string addOutput = RunGitCommand(directory, "add .");
+            if (!string.IsNullOrEmpty(addOutput))
+            {
+                MessageBox.Show("파일 추가 오류: " + addOutput);
+                return;
+            }
+
+            string commitOutput = RunGitCommand(directory, $"commit -m \"{commitMessage}\"");
+            if (string.IsNullOrEmpty(commitOutput))
+            {
+                MessageBox.Show("커밋 실패: 오류 발생");
+                return;
+            }
+
+            MessageBox.Show("커밋 완료!");
+
+           
+        }
+
+        // Git 명령어 실행 함수
+        private string RunGitCommand(string directory, string args)
+        {
+            ProcessStartInfo psi = new ProcessStartInfo
+            {
+                FileName = "git",
+                Arguments = args,
+                WorkingDirectory = directory,  // 작업할 디렉토리 지정
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            using (Process process = new Process { StartInfo = psi })
+            {
+                process.Start();
+                string output = process.StandardOutput.ReadToEnd();
+                string error = process.StandardError.ReadToEnd();
+                process.WaitForExit();
+
+                if (!string.IsNullOrEmpty(error))
+                {
+                    return "Error: " + error;
+                }
+                return output;
+            }
+        }
+
+        // Git 저장소 여부 확인
+        private bool IsGitRepository(string directory)
+        {
+            string gitCheckOutput = RunGitCommand(directory, "rev-parse --is-inside-work-tree");
+            return gitCheckOutput.Trim() == "true";
+        }
+
+        // Git 저장소 초기화
+        private void InitializeGitRepository(string directory)
+        {
+            string initOutput = RunGitCommand(directory, "init");
+            if (!string.IsNullOrEmpty(initOutput))
+            {
+                MessageBox.Show("Git 저장소 초기화 실패: " + initOutput);
+            }
+            else
+            {
+                MessageBox.Show("Git 저장소가 초기화되었습니다.");
+            }
+        }
+    }
+}
